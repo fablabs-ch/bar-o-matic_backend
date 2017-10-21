9ee15aac6a2e7c6ad3d661a6601b23df6aa8497a
gaetancollaud <gaetancollaud@gmail.com>
2017-10-21 19:01:41 +0200

    fix docker

--- src/main/java/ch/fablabs/fabjam/cocktail/service/ServoServiceMock.java
+++ src/main/java/ch/fablabs/fabjam/cocktail/service/ServoServiceMock.java
@@ -1,11 +1,8 @@
 package ch.fablabs.fabjam.cocktail.service;
 
-import ch.fablabs.fabjam.cocktail.driver.PWMDevice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
 
 @Slf4j
 @Service
